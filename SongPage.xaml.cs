cba1af95c29af276470416b452bedffe1f7638f6
Adrien Allard <adrien.allard.pro@gmail.com>
2023-10-14 13:34:12 +0200

    Rework metronome
    - Move it in ViewModel
    - Use BackgroundWorker instead of Task api
    - Make it react to light/dark theme

--- SongPage.xaml.cs
+++ SongPage.xaml.cs
@@ -10,17 +10,6 @@ namespace SongPrompter
     {
         public const string PlaylistParameterName = "playlist";
 
-        private readonly Color defaultColor = new Color(1f);
-        private readonly Color flashColor = new Color(0.5f);
-
-        private readonly TimeSpan flashDuration = TimeSpan.FromMilliseconds(50);
-        private readonly TimeSpan threadWakeupDuration = TimeSpan.FromMilliseconds(15);
-
-        private bool scroll = false;
-
-        private volatile bool exitUpdate;
-        private Task updateTask;
-
         public SongPage(SongViewModel viewModel)
         {
             InitializeComponent();
@@ -32,75 +21,9 @@ namespace SongPrompter
             get => ((SongViewModel)this.BindingContext).Playlist;
             set
             {
-                if (this.updateTask != null)
-                {
-                    this.exitUpdate = true;
-                    this.updateTask.Wait();
-                }
-
                 ((SongViewModel)this.BindingContext).Bind(value);
-                
-                this.updateTask = Task.Run(this.Update);
-
                 OnPropertyChanged();
             }
-        }
-
-        private void Update()
-        {
-            Stopwatch stopwatch = Stopwatch.StartNew();
-            Action flashAction = () => this.metronome.BackgroundColor = this.flashColor;
-            Action resetAction = () => this.metronome.BackgroundColor = this.defaultColor;
-            Action scrollAction = () => this.scrollView.ScrollToAsync(0, this.scrollView.ScrollY + 10, true);
-
-            TimeSpan timeElapsedUntilLastBeat = TimeSpan.Zero;
-
-            Song currentSong = ((SongViewModel)this.BindingContext).CurrentSong;
-            if (currentSong == null)
-            {
-                return;
-            }
-
-            TimeSpan beatDuration = TimeSpan.FromMinutes(1.0 / currentSong.Bpm);
-            while (!this.exitUpdate)
-            {
-                var timeElapsed = stopwatch.Elapsed;
-                var durationSinceLastBeat = timeElapsed - timeElapsedUntilLastBeat;
-                if (durationSinceLastBeat >= beatDuration) 
-                {
-                    var lateness = durationSinceLastBeat - beatDuration;
-
-                    timeElapsedUntilLastBeat = timeElapsed;
-                    if (lateness < beatDuration)
-                    {
-                        // Fix up latencies.
-                        timeElapsedUntilLastBeat -= lateness;
-                    }
-
-                    Dispatcher.Dispatch(flashAction);
-                    Thread.Sleep(this.flashDuration);
-                    Dispatcher.Dispatch(resetAction);
-
-                    if (this.scroll)
-                    {
-                        Dispatcher.Dispatch(scrollAction);
-                    }
-                }
-                else
-                {
-                    var durationFromNextBeat = beatDuration - durationSinceLastBeat - this.threadWakeupDuration;
-                    if (durationFromNextBeat > TimeSpan.Zero)
-                    {
-                        Thread.Sleep(durationFromNextBeat);
-                    }
-                }
-            }
-        }
-
-        private void StartButton_Clicked(object sender, EventArgs e)
-        {
-            //this.scrollView.ScrollToAsync(this.lyrics, ScrollToPosition.Center, true);
-            this.scroll = true;
         }
     }
 }
--- ViewModels/SongViewModel.cs
+++ ViewModels/SongViewModel.cs
@@ -2,11 +2,18 @@
 using System.Diagnostics;
 
 using SongPrompter.Models;
+using System.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace SongPrompter.ViewModels
 {
     public partial class SongViewModel : ObservableObject
     {
+        private readonly Style defaultStyle;
+        private readonly Style flashStyle;
+        private readonly TimeSpan flashDuration = TimeSpan.FromMilliseconds(50);
+        private readonly TimeSpan threadWakeupDuration = TimeSpan.FromMilliseconds(15);
+
         [ObservableProperty]
         private Playlist playlist;
 
@@ -14,12 +21,39 @@ namespace SongPrompter.ViewModels
         private Song currentSong;
 
         [ObservableProperty]
+        private string infos;
+
+        [ObservableProperty]
         private string lyrics;
+
+        [ObservableProperty]
+        private Style metronomeStyle;
+
+        [ObservableProperty]
+        private int beat;
 
         public int nextSongIndex = 0;
 
+        private readonly BackgroundWorker backgroundWorker;
+
         public SongViewModel()
         {
+            if (App.Current.Resources.TryGetValue("LabelStyle", out var style))
+            {
+                this.defaultStyle = (Style)style;
+            }
+
+            if (App.Current.Resources.TryGetValue("MetronomeFlashStyle", out style))
+            {
+                this.flashStyle = (Style)style;
+            }
+
+            this.backgroundWorker = new BackgroundWorker
+            {
+                WorkerSupportsCancellation = true
+            };
+
+            this.backgroundWorker.DoWork += this.BackgroundWorker_DoWork;
         }
 
         internal void Bind(Playlist playlist)
@@ -34,10 +68,73 @@ namespace SongPrompter.ViewModels
             Debug.Assert(this.Playlist != null);
             if (this.nextSongIndex < this.Playlist.Songs.Length)
             {
+                this.backgroundWorker.CancelAsync();
+                while (this.backgroundWorker.IsBusy)
+                {
+                    Thread.Sleep(1);
+                }
+
                 this.CurrentSong = this.Playlist.Songs[this.nextSongIndex];
+                this.Infos = $"{this.CurrentSong.Bpm} bpm    Signature: {this.CurrentSong.BeatPerMeasure}/{this.CurrentSong.BeatSubdivision}    Key: {this.CurrentSong.Key}";
                 this.Lyrics = this.CurrentSong.Lyrics.Aggregate((left, right) => left + '\n' + right);
                 this.nextSongIndex++;
+
+                this.backgroundWorker.RunWorkerAsync();
             }
+        }
+
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            TimeSpan timeElapsedUntilLastBeat = TimeSpan.Zero;
+
+            this.Beat = 0;
+            TimeSpan beatDuration = TimeSpan.FromMinutes(1.0 / this.CurrentSong.Bpm);
+            while (!worker.CancellationPending)
+            {
+                var timeElapsed = stopwatch.Elapsed;
+                var durationSinceLastBeat = timeElapsed - timeElapsedUntilLastBeat;
+                if (durationSinceLastBeat >= beatDuration)
+                {
+                    var lateness = durationSinceLastBeat - beatDuration;
+
+                    timeElapsedUntilLastBeat = timeElapsed;
+                    if (lateness < beatDuration)
+                    {
+                        // Fix up latencies.
+                        timeElapsedUntilLastBeat -= lateness;
+                    }
+
+                    this.MetronomeStyle = this.flashStyle;
+                    this.Beat++;
+
+                    Thread.Sleep(this.flashDuration);
+
+                    this.MetronomeStyle = this.defaultStyle;
+
+                    //if (this.scroll)
+                    //{
+                    //    Dispatcher.Dispatch(scrollAction);
+                    //}
+                }
+                else
+                {
+                    var durationFromNextBeat = beatDuration - durationSinceLastBeat - this.threadWakeupDuration;
+                    if (durationFromNextBeat > TimeSpan.Zero)
+                    {
+                        Thread.Sleep(durationFromNextBeat);
+                    }
+                }
+            }
+        }
+
+        [RelayCommand]
+        private async void Start()
+        {
+            this.Beat = 0;
         }
     }
 }
